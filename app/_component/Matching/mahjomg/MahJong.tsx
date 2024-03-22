9bd6aa208e5e49789c913bdc342a1368360a7f05
Jacky Hong <pkh_jackyhong@photons.com.tw>
2024-03-22 15:22:45 +0800

    汇入配牌功能

--- app/_component/Matching/mahjomg/MahJong.tsx
+++ app/_component/Matching/mahjomg/MahJong.tsx
@@ -1,16 +1,112 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import { Card, createAllCard, CardType, DragState } from "../../../_lib/card/mahJong/Card";
 import DrapPicture from "./DrapPicture";
 import BoardSquare from "./BoardSquare";
 import styled from "./MahJong.module.css";
 import { PlusSquareOutlined, ReloadOutlined } from "@ant-design/icons";
-import { Col, Row, Button, Flex, Tag, Modal, Tooltip } from "antd";
+import { Col, Row, Button, Flex, message, Modal, Tooltip,Input,FloatButton   } from "antd";
 import { paiCard } from "../../../_lib/card/mahJong/paiCard";
 import Radio from "antd/lib/radio";
 import { useCurrControlPlayerList } from "../../../store/mahJongStore";
 import { usePlayerList, useCardPool } from "../../../store/mahJongStore";
-export const DraggableCard = () => {};
+import { extractContentFromTags,getCardDataAndTranArr } from "@/app/_lib/card/mahJong/ReadPaiCard";
+
+
+const OverWritePaiCardButton: React.FC = () => {
+
+    const { TextArea } = Input;
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [messageApi, contextHolder] = message.useMessage(); 
+
+
+    const paiCardTxt = useRef<any>(null)
+
+    
+    let { setPlayerCard }  = usePlayerList();
+    let { setCardPool } = useCardPool();
+                
+
+    const showModal = () => {
+        setIsModalOpen(true);
+
+    };
+
+    const handleOk = () => {
+
+        
+        if (paiCardTxt.current) {
+            let getTxt = paiCardTxt.current.resizableTextArea.textArea.value 
+            let templete = `<templete>00</templete>` + getTxt  // `<templete>00</templete>` 让正规表达式侦测的样板  20240322 测试
+
+            let getPaiCardArrResult = extractContentFromTags(getTxt)
+
+            if(typeof  getCardDataAndTranArr(getPaiCardArrResult) == "string" || !getCardDataAndTranArr(getPaiCardArrResult)){
+                messageApi.open({
+                    type: 'error',
+                    content: '格式不正确你要配个毛啊 操'
+                });
+            }else{  
+
+                let { cardPoolResultList,player0ResultList,player1ResultList,player2ResultList,player3ResultList } = getCardDataAndTranArr(getPaiCardArrResult) as unknown as any
+                    setPlayerCard(0,player0ResultList)
+                    setPlayerCard(1,player1ResultList)
+                    setPlayerCard(2,player2ResultList)
+                    setPlayerCard(3,player3ResultList)
+                    setCardPool(cardPoolResultList)
+
+                    messageApi.open({
+                        type: 'success',
+                        content: '配牌成功'
+                    });
+                    
+
+                    // setTimeout(()=>{
+                    //     setIsModalOpen(false);
+                    // },100) 
+            }
+
+
+
+
+            
+
+
+        }
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+
+
+    
+
+    return (
+        <>
+            <Modal
+                title="放入配牌文挡"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                okText="开始配牌"
+                cancelText="取消">
+                <TextArea rows={12} placeholder="maxLength is 6" ref={paiCardTxt}/>
+                {contextHolder}
+            </Modal>
+
+
+
+            <FloatButton onClick={showModal} />
+        </>
+    );
+}
+
+
+
 const MahJong = () => {
     // zustand store data
     
@@ -25,7 +121,6 @@ const MahJong = () => {
         sortCardList,
     } = useCardPool();
 
-   
     // banker
     let [banker, setBanker] = useState<number>(1);
 
@@ -44,7 +139,7 @@ const MahJong = () => {
         setIsModalOpen(false);
     };
 
-    // ==== banker ====
+    const [messageApi, contextHolder] = message.useMessage(); 
 
 
 
@@ -54,8 +149,12 @@ const MahJong = () => {
             removeCardAtCardList(card)
             setCardGragState(currControl,card,DragState.CANNOTDRAG)
             sortPlayerCard(currControl);
-        }   
-      
+        }else{
+            messageApi.open({
+                type: 'warning',
+                content: '玩家手牌以补满无法新增'
+            });
+        }
     }
 
 
@@ -177,6 +276,7 @@ const MahJong = () => {
                 onCancel={handleCancel}
             >
                 <p>
+                    
                     {paiCard(
                         playerCardList[0],
                         playerCardList[1],
@@ -187,6 +287,8 @@ const MahJong = () => {
                     )}
                 </p>
             </Modal>
+            {contextHolder}
+            <OverWritePaiCardButton/>
         </>
     );
 };
